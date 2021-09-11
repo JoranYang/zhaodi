5946036ac6c1548bf8f75f017f435459d0f7d2be
JoranYang <124832333@qq.com>
2021-09-11 12:25:27 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,49 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// #ifdef APP-PLUS  
+			// 监听APP推送消息
+			plus.push.addEventListener('click', function(message) {});
+			plus.push.addEventListener('receive', function(message) {
+				const payload = JSON.parse(message.payload)
+				console.log('payload:', payload)
+				const type = payload.type
+				switch (type) {
+					case 1:
+						uni.navigateTo({
+							url: '/pages/px/community/detail?id=' + payload.id
+						})
+						break
+					case 3:
+						uni.navigateTo({
+							url: '/pages/kh/goods/info?id=' + payload.shipAppId + '&content=null'
+						})
+						break
+					case 5:
+						uni.navigateTo({
+							url: '/pages/kh/goods/test?current=2'
+						})
+						break
+				}
+			})
+			// #endif 
+		}
+	}
+</script>
+
+<style lang="scss">
+	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+	@import "uview-ui/index.scss";
+	/*每个页面公共css */
+	@import "./colorui/main.css";
+	@import "./static/font/iconfont.css";
+
+	@font-face {
+		font-family: "siyuan";
+		src: url("/static/font/siyuan.ttf")
+	}
+
+	.siyuan {
+		font-family: 'siyuan' !important;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App'
+import common from './util/common.js'
+import moment from 'moment'
+import base_url from './util/baseUrl.js'
+import filters from '@/filter/index.js'
+
+
+Vue.config.productionTip = false 
+
+
+
+moment.locale('zh-cn') // 设置语言 或 moment.lang('zh-cn');
+Vue.prototype.$moment = moment // 赋值使用
+
+Vue.prototype.$common = common // 赋值使用
+
+Vue.prototype.$baseUrl = base_url
+
+App.mpType = 'app'
+
+import store from '@/store'
+// 引入uView提供的对vuex的简写法文件
+let vuexStore = require('@/store/$u.mixin.js')
+Vue.mixin(vuexStore)
+
+Object.keys(filters).forEach(key => {
+	Vue.filter(key, filters[key]) //插入过滤器名和对应方法
+})
+
+import uView from "uview-ui";
+Vue.use(uView);
+
+//#ifdef APP-PLUS
+plus.runtime.getProperty(plus.runtime.appid, function(widgetInfo) {
+	Vue.prototype.$version = widgetInfo.version
+})
+// #endif
+const app = new Vue({
+	store,
+	...App
+})
+
+app.$mount()
+export default app
